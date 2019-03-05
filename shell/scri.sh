4e8a39182e9d16d56974e7c12081d9a31e93544b
srinath116 <srinathgunnu151296@gmail.com>
2019-03-06 01:32:00 +0530

    fisrt commit

---
+++ shell/scri.sh
@@ -0,0 +1,7 @@
+
+#!/bin/sh
+echo "What is your name?"
+read USER_NAME
+echo "Hello $USER_NAME"
+echo "I will create you a file called $USER_NAME_file"
+touch $USER_NAME
---
+++ shell/script2.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+# Address Book
+
+
+trap 'do_menu' 2
+
+. ./addr_libs.sh
+
+show_menu()
+{
+  # Called by do_menu
+  
+  echo
+  echo
+  echo
+  echo
+  echo "-- Address Book --"
+  echo "1. List / Search"
+  echo "2. Add"
+  echo "3. Edit"
+  echo "4. Remove"
+  echo "q. Quit"
+  echo -en "Enter your selection: "
+}
+
+do_menu()
+{
+  i=-1
+
+  while [ "$i" != "q" ]; do
+    show_menu
+    read i
+    i=`echo $i | tr '[A-Z]' '[a-z]'`
+    case "$i" in 
+	"1")
+	list_items
+	;;
+	"2")
+	add_item
+	;;
+	"3")
+	edit_item
+	;;
+	"4")
+	remove_item
+	;;
+	"q")
+	# Ought to confirm before quitting!
+	echo "quitting."
+	exit 0
+	;;
+	*)
+	echo "Unrecognised input."
+	;;
+    esac
+  done
+}
+
+##########################################################
+############ Main script starts here #####################
+##########################################################
+
+if [ ! -f $BOOK ]; then
+  echo "Creating $BOOK ..."
+  touch $BOOK
+fi
+
+if [ ! -r $BOOK ]; then
+  echo "Error: $BOOK not readable"
+  exit 1
+fi
+
+if [ ! -w $BOOK ]; then
+  echo "Error: $BOOK not writeable"
+  exit 2
+fi
+
+do_menu
